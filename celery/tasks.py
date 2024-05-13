1967414a4eb695f94c01c02f6450fe57af1abbb4
najib37 <najibramadan37@gmail.com>
2024-05-13 16:52:35 -0400

    added a celery task to convert csv to json

--- celery/tasks.py
+++ celery/tasks.py
@@ -1,18 +1,28 @@
 
-from time import sleep
 from celery import Celery
+from pathlib import Path
+import csv
+import json
 
-# app = Celery (
-#     'tasks',
-#     # backend='redis://redis:6060',
-#     # broker="amqp://userf:userd@localhost:5672"
-#     # TODO: these needs to be replaced by env variables 
-# )
-#
-# @app.task(name='demo')
-# def hello():
-#     sleep(5)
-#     print("recieved")
-#     return "hello"
+BASEDIR = Path(__file__).resolve().parent.parent
 
-# def processFile
+app = Celery (
+    'tasks',
+    backend='redis://redis:6379',
+    broker="amqp://userf:userd@rabbitmq:5672"
+    # TODO: these needs to be replaced by env variables 
+)
+
+@app.task(name='converter')
+def converter(filename):
+    jsonfile = open('test.json', 'w') 
+
+    # some hacks but the point is to iterate on the data and send the progress using rabbitmq
+    jsonfile.write('[\n')
+
+    with open(filename, 'r', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            json.dump(row, jsonfile)
+            jsonfile.write(',\n')
+    jsonfile.write(']')

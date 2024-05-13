46119b1fc68bbda5793e91c6d2b29c1bbf3a3262
najib37 <najibramadan37@gmail.com>
2024-05-13 19:32:28 -0400

    updated the celery tasks to convert the the csv uploaded file to json

--- celery/tasks.py
+++ celery/tasks.py
@@ -4,8 +4,6 @@ from pathlib import Path
 import csv
 import json
 
-BASEDIR = Path(__file__).resolve().parent.parent
-
 app = Celery (
     'tasks',
     backend='redis://redis:6379',
@@ -14,15 +12,24 @@ app = Celery (
 )
 
 @app.task(name='converter')
-def converter(filename):
-    jsonfile = open('test.json', 'w') 
+def converter(filename: str):
 
-    # some hacks but the point is to iterate on the data and send the progress using rabbitmq
+    # this file is full of hacks but the point is to async convert the file 
+    # and send the progress to the user
+    filename = "./media/" + filename
+    jsonFileName = filename.replace('.csv', ".json")
+
+    jsonfile = open(jsonFileName, 'w') 
+
+    print(f'filename = {filename}')
+    print(f'jsonfile = {jsonFileName}')
     jsonfile.write('[\n')
-
     with open(filename, 'r', newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             json.dump(row, jsonfile)
             jsonfile.write(',\n')
     jsonfile.write(']')
+    #########################
+
+    return "success"
